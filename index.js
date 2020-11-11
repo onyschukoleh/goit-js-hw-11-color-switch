d16dbd7f969ff80638ec8f6e930b3739c6ca8ad5
onyschukoleh <onyschukoleh@gmail.com>
2020-11-11 09:26:47 +0200

    Add .prettierrc

--- index.js
+++ index.js
@@ -1,49 +1,42 @@
-
-const colors = 
-[
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
 ];
+const refs = {
+  startBtn: document.querySelector('.start'),
+  stopBtn: document.querySelector('.stop'),
+};
 
-  const refs = 
-{
-    startBtn : document.querySelector (".start"),
-    stopBtn : document.querySelector (".stop")  
+const hendelStop = () => {
+  refs.stopBtn.setAttribute('disabled', true);
+  refs.startBtn.removeAttribute('disabled');
+};
+
+refs.stopBtn.addEventListener('click', hendelStop);
+
+const hendeldStart = () => {
+  colorSwich();
+};
+
+refs.startBtn.addEventListener('click', hendeldStart);
+
+function colorSwich() {
+  const id = setInterval(() => {
+    document.body.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, 5)];
+  }, 1000);
+
+  refs.stopBtn.addEventListener('click', () => {
+    clearInterval(id);
+  });
+  refs.startBtn.setAttribute('disabled', true);
+  refs.stopBtn.removeAttribute('disabled');
 }
 
-const hendelStop = () => 
-{
-    refs.stopBtn.setAttribute ("disabled",true)
-    refs.startBtn.removeAttribute ("disabled") 
-}
-
-refs.stopBtn.addEventListener ('click', hendelStop)
-
-const hendeldStart = () => 
-{
-    colorSwich ()  
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
-refs.startBtn.addEventListener ('click', hendeldStart);
-
-function colorSwich () 
-{
-    const id = setInterval(()=>
-    {
-        document.body.style.backgroundColor = colors[randomIntegerFromInterval(0,5)]
-    },1000)
-    
-    refs.stopBtn.addEventListener('click',()=>{clearInterval(id)})
-    refs.startBtn.setAttribute ("disabled",true)
-    refs.stopBtn.removeAttribute ("disabled")
-};
-
-const randomIntegerFromInterval = (min, max) => 
-{
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
